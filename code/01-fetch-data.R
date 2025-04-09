59d30836993e358f20fc75e88e41163b5c5af46a
Rodrigo Granjel <granjel@gmail.com>
2025-04-09 20:27:39 +0200

    initialized repo, wrote code to download data from Google sheets and format it, then made some preliminary plots with the first round; still some #todo related to the likely addition of new levels for treatments bti and food

---
+++ code/01-fetch-data.R
@@ -0,0 +1,54 @@
+# retrieve data from Google Drive ----------------------------------------
+
+# load packages
+library(googledrive)
+library(tidyverse)
+library(readxl)
+
+# download data
+drive_find(pattern = "mosquito-bti", type = "spreadsheet") %>%
+  drive_download(
+    type = "xlsx",
+    path = "data/mosquito-bti.xlsx",
+    overwrite = TRUE
+  )
+
+# load data set
+data <- readxl::read_xlsx("data/mosquito-bti.xlsx", sheet = 1) %>%
+  mutate(
+    # create id tag for each row to identify them more easily
+    id = paste(date, mosquito, bti, food, n, reading, sep = "_"),
+    # create a column with the combination of treatments
+    treatment = paste(bti, food, sep = "_"),
+    # date and time need to be fixed for R
+    date = as.Date(date),
+    time = as.POSIXct(paste(date, format(time, "%H:%M:%S")), tz = "UTC"),
+    # order bti levels # todo change if new levels are present
+    bti = factor(
+      bti,
+      levels = c("control", "high"),
+      labels = c("Control", "High")
+    ),
+    # order food levels # todo change if new levels are present
+    food = factor(food, levels = c("low", "high"), labels = c("Low", "High")),
+    # calculate percentage of dead individuals
+    perc = (dead / (dead + alive)) * 100
+  ) %>%
+  # rename time because it also has the date within
+  rename(datetime = time) %>%
+  # rearrange the order of the columns
+  relocate(
+    type,
+    date,
+    datetime,
+    mosquito,
+    bti,
+    food,
+    treatment,
+    n,
+    reading,
+    alive,
+    dead,
+    perc,
+    id
+  )
---
+++ code/2025-04-09-prelim-plots.R
@@ -0,0 +1,66 @@
+# make some preliminary plots --------------------------------------------
+
+# load data through the relevant script
+source("code/01-fetch-data.R")
+
+# load colors
+source("tools/mosquito-colors.R")
+
+# define theme for all plots
+theme_set(theme_bw() + theme(strip.background = element_blank()))
+
+
+# paths in time ----------------------------------------------------------
+
+p1 <- data %>%
+  filter(reading != 24) %>% # todo remove this line after getting the results
+  ggplot(aes(x = reading, y = perc, color = bti, group = n)) +
+  facet_grid(
+    food ~ bti
+  ) +
+  geom_point() +
+  geom_path() +
+  labs(x = "Experiment time (hours)", y = "Dead individuals (%)") +
+  scale_color_manual(values = mosquito_colors) +
+  scale_y_continuous(
+    sec.axis = sec_axis(~., name = "Food", breaks = NULL, labels = NULL)
+  ) +
+  scale_x_continuous(
+    sec.axis = sec_axis(~., name = "BTi", breaks = NULL, labels = NULL)
+  ) +
+  theme(legend.position = "none")
+
+ggsave(
+  p1,
+  filename = paste0("figures/", today(), "-paths.png"),
+  dpi = 320,
+  width = 5,
+  height = 3,
+  device = "png"
+)
+
+# barplots ---------------------------------------------------------------
+
+p2 <- data %>%
+  ggplot(aes(x = food, y = perc, fill = bti)) +
+  facet_grid(
+    . ~ bti,
+    labeller = labeller(
+      # todo change if more levels are added
+      bti = c("Control" = "BTi: Control", "High" = "BTi: High")
+    )
+  ) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(x = "Food", y = "Dead individuals (%)") +
+  scale_fill_manual(values = mosquito_colors) +
+  scale_y_continuous(expand = expansion(mult = c(0, 0.05))) +
+  theme(legend.position = "none")
+
+ggsave(
+  p2,
+  filename = paste0("figures/", today(), "-barplot.png"),
+  dpi = 320,
+  width = 5,
+  height = 3,
+  device = "png"
+)

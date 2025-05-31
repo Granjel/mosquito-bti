7c483e2d9c9e4d4957ffd999b2bdba0310d107ca
Rodrigo Granjel <granjel@gmail.com>
2025-05-31 10:50:32 +0200

    :+1: improved dpi of figures and created a 24-48h boxplot figure

--- sertraline/sertraline-code.R
+++ sertraline/sertraline-code.R
@@ -14,7 +14,7 @@ drive_find(pattern = "sertraline_emraas", type = "spreadsheet") %>%
   )
 
 # load data and make a few adjustments
-data <- readxl::read_xlsx("data/sertraline-raw.xlsx", sheet = "data") %>%
+data <- readxl::read_xlsx("sertraline/sertraline-raw.xlsx", sheet = "data") %>%
   mutate(
     date = as.Date(date, format = "%d/%m/%Y"), # change date format
     dead = (5 - alive) / 5 * 100, # dead (% of the initial number)
@@ -27,7 +27,7 @@ species <- unique(data$species)
 
 # clean df (remove unnecessary variables)
 data <- data %>%
-  select(date, time, dose, n, alive, dead) # we don't need columns like species or reading
+  select(date, time, dose, n, alive, deceased, dead) # we don't need columns like species or reading
 
 
 # plots ------------------------------------------------------------------
@@ -79,7 +79,7 @@ sertraline_plot <- data %>%
 ggsave(
   sertraline_plot,
   filename = "sertraline/sertraline.png",
-  dpi = 320,
+  dpi = 640,
   width = 12,
   height = 8,
   device = "png"
@@ -118,8 +118,42 @@ sertraline_plot_48h <- data48 %>%
 ggsave(
   sertraline_plot_48h,
   filename = "sertraline/sertraline-48h.png",
-  dpi = 320,
+  dpi = 640,
   width = 10,
   height = 8,
+  device = "png"
+)
+
+# boxplots 48 hours
+sum_data <- data %>%
+  filter(time %in% c(24, 48))
+
+sertraline_boxplot <- sum_data %>%
+  ggplot(aes(x = time, y = dead, group = time, color = dose, fill = dose)) +
+  facet_grid(~dose) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.5) +
+  scale_fill_viridis_d(begin = 0.1, end = 0.9) +
+  scale_color_viridis_d(begin = 0.1, end = 0.9) +
+  scale_x_continuous(
+    breaks = c(24, 48),
+    sec.axis = sec_axis(
+      ~.,
+      name = "Dose of sertraline (μg/L)",
+      breaks = NULL,
+      labels = NULL
+    )
+  ) +
+  labs(
+    x = "Elapsed time (hours)",
+    y = "Dead individuals (%)"
+  ) +
+  theme(panel.grid.major.x = element_blank())
+
+ggsave(
+  sertraline_boxplot,
+  filename = "sertraline/sertraline-boxplot.png",
+  dpi = 640,
+  width = 10,
+  height = 6,
   device = "png"
 )

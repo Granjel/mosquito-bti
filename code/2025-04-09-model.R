b9b98b6592aa876263a15a66b96e70186df35fdf
Rodrigo Granjel <granjel@gmail.com>
2025-08-23 19:40:06 +0200

    nothing major

--- code/2025-04-09-model.R
+++ code/2025-04-09-model.R
@@ -1,9 +1,11 @@
 # analysis idea ----------------------------------------------------------
 
-# need more replicates per treatment (6 min, 10 ideally)
 library(lme4)
 model <- glmer(
-  cbind(dead, alive) ~ bti * food * reading + (1 | jar),
+  cbind(dead, alive) ~ bti * food * reading + (1 | experiment) + (1 | unit),
   family = binomial,
   data = data
 )
+
+
+summary(model)

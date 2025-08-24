582947b64f504ea4670da5e5513febc15ac68d12
Rodrigo Granjel <granjel@gmail.com>
2025-08-24 02:41:00 +0200

    only two colors now, because of faceting

--- tools/bti-colors.R
+++ tools/bti-colors.R
@@ -4,10 +4,17 @@
 # https://emilhvitfeldt.github.io/r-color-palettes/discrete/PNWColors/Bay/
 pacman::p_load("paletteer")
 
-# define colors per treatment
-lvl <- c("No_Low", "Yes_Low", "No_High", "Yes_High")
+# # define colors per treatment
+# lvl <- c("No_Low", "Yes_Low", "No_High", "Yes_High")
+
+# pal <- setNames(
+#   paletteer_d("PNWColors::Bay")[c(2, 1, 4, 5)],
+#   lvl
+# )
+
+lvl <- c("No", "Yes")
 
 pal <- setNames(
-  paletteer_d("PNWColors::Bay")[c(2, 1, 4, 5)],
+  paletteer_d("PNWColors::Bay")[c(2, 4)],
   lvl
 )
